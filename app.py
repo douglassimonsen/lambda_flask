c1514f2ffdbeee818b9ce854d5474b4a6cb69f36
Matthew Hamilton <daedalus1432@gmail.com>
2022-07-13 15:48:25 -0500

    Adding more functionality

--- app.py
+++ app.py
@@ -15,8 +15,53 @@ def test1():
 
 @app.route('/help')
 def test2():
+    print(flask.request.get_json())
     return 'b'
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+evt = {
+        "version": "2.0",
+        "routeKey": "ANY /{proxy+}",
+        "rawPath": "/help",
+        "rawQueryString": "",
+        "headers": {
+            "accept": "*/*",
+            "accept-encoding": "gzip, deflate",
+            "content-length": "6",
+            "content-type": "application/x-www-form-urlencoded",
+            "host": "wmsqb2vrw7.execute-api.us-east-1.amazonaws.com",
+            "user-agent": "python-requests/2.27.1",
+            "x-amzn-trace-id": "Root=1-62cf2ad6-08dcd67862f1b6390676ade2",
+            "x-forwarded-for": "98.46.0.170",
+            "x-forwarded-port": "443",
+            "x-forwarded-proto": "https"
+        },
+        "requestContext": {
+            "accountId": "111263457661",
+            "apiId": "wmsqb2vrw7",
+            "domainName": "wmsqb2vrw7.execute-api.us-east-1.amazonaws.com",
+            "domainPrefix": "wmsqb2vrw7",
+            "http": {
+                "method": "POST",
+                "path": "/debug",
+                "protocol": "HTTP/1.1",
+                "sourceIp": "98.46.0.170",
+                "userAgent": "python-requests/2.27.1"
+            },
+            "requestId": "VOOhlhv9oAMEMHQ=",
+            "routeKey": "ANY /{proxy+}",
+            "stage": "$default",
+            "time": "13/Jul/2022:20:28:06 +0000",
+            "timeEpoch": 1657744086756
+        },
+        "pathParameters": {
+            "proxy": "debug"
+        },
+        "body": "aGk9Ynll",
+        "isBase64Encoded": True
+    }
+
+print(app(evt, {}))
--- lambda_flask/lambda_flask.py
+++ lambda_flask/lambda_flask.py
@@ -1,22 +1,43 @@
-from . import flask_json as _json
+from . import flask_json, utils
 import json
+import base64
+import urllib.parse
 CORS_HEADERS = {
     "Access-Control-Allow-Headers" : "Content-Type",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*"
 };
 
+
+def jsonify(obj):
+    return obj
+
+
+class request:
+    @staticmethod
+    def get_json():
+        return {}
+
+
 class Flask:
     def __init__(self, name) -> None:
         self.name = name
         self.routes = {}
-        self.json_encoder = _json.JSONEncoder()
+        self.json_encoder = flask_json.JSONEncoder()
+        self.evt = None
+        self.context = None
+        utils.request.get_json = self.tmp_get_json  # necessary to get flask functionality. Most likely to cause threading weirdness
 
     def __call__(self, evt, context) -> dict:
         """
         This function is the entrypoint for the lambda function
         """
-        resp = self.exec_route(evt, context)
+        if evt['isBase64Encoded']:
+            evt['body'] = base64.b64decode(evt['body']).decode("utf-8")
+        self.evt = evt
+        self.context = context
+
+        resp = self.exec_route()
         return self.CORS(resp)
 
     def route(self, raw_path):
@@ -37,14 +58,26 @@ class Flask:
         msg['headers'] = headers
         return msg
 
-    def exec_route(self, evt, context):
-        raw_path = evt['rawPath']
+    def tmp_get_json(self):
+        method = self.evt['requestContext']['http']['method']
+        if method == 'GET':
+            return self.evt['queryStringParameters']
+        else:
+            try:
+                return json.loads(self.evt['body'])
+            except json.decoder.JSONDecodeError:
+                body = urllib.parse.parse_qs(self.evt['body'])
+                return {k: v[0] for k, v in body.items()}
+        return method
+
+    def exec_route(self):
+        raw_path = self.evt['rawPath']
         if raw_path == '/debug':  # special method to help with debugging, almost certainly a security vulnerability
             return {
                 'statusCode': 200,
                 'body': self.json_encoder.default({
-                    'evt': evt,
-                    'context': context,
+                    'evt': self.evt,
+                    'context': self.context,
                 })
             }
 
@@ -53,7 +86,6 @@ class Flask:
                 "statusCode": 404,
                 "body": f"The path '{raw_path}' could not be found"
             }
-        
         try:
             resp = self.routes[raw_path]()
         except Exception as e:
